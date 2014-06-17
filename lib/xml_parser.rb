8ee3c7313d45dba3eb8d1ce6acccdfec8fdc48b9
calvached <calvache.diana@gmail.com>
2014-06-17 13:59:32 -0500

    Completing XmlParser

---
+++ lib/xml_parser.rb
@@ -0,0 +1,29 @@
+class XmlParser
+  def parse_xml(file)
+    xml_string = read(file)
+    parsed_data = []
+
+    parse_rates(xml_string).each do |row|
+      data_set = extract_data_sets(row)
+      parsed_data << format_rate(data_set)
+    end
+
+    parsed_data
+  end
+
+  def format_rate(data_set)
+    Hash[*data_set.flatten]
+  end
+
+  def extract_data_sets(row)
+    row[0].scan(/<(\w+)>(\w+\.?\d*)/)
+  end
+
+  def parse_rates(string)
+    string.scan(/<rate>(.*?)<\/rate>/m)
+  end
+
+  def read(file)
+    File.read(file)
+  end
+end
---
+++ spec/xml_parser_spec.rb
@@ -0,0 +1,53 @@
+require 'xml_parser'
+
+describe XmlParser do
+
+  it 'reads a file' do
+   parser  = XmlParser.new
+   expect(parser.read('spec/rates.xml')).to be_a(String)
+  end
+
+  it 'returns each set of rates' do
+    parser = XmlParser.new
+    test_rates =  '<rate>
+        <from>AUD</from>
+            <to>CAD</to>
+                <conversion>1.0079</conversion>
+                  </rate>
+                    <rate>
+    <from>AUD</from>
+    <to>EUR</to>
+    <conversion>0.7439</conversion>
+    </rate>'
+
+    expect(parser.parse_rates(test_rates)).to eq([["\n        <from>AUD</from>\n            <to>CAD</to>\n                <conversion>1.0079</conversion>\n                  "], ["\n    <from>AUD</from>\n    <to>EUR</to>\n    <conversion>0.7439</conversion>\n    "]])
+  end
+
+  it 'formats a row of data' do
+    parser = XmlParser.new
+    test_row = [["from", "AUD"], ["to", "CAD"], ["conversion", "1.0079"]]
+
+    expect(parser.format_rate(test_row)).to eq({"from"=>"AUD", "to"=>"CAD", "conversion"=>"1.0079"})
+  end
+
+  it 'extracts data labels with their cooresponding values' do
+    parser = XmlParser.new
+    test_row = ["\n\t\t<from>AUD</from>\n\t\t<to>CAD</to>\n\t\t<conversion>1.0079</conversion>\n\t"]
+
+    expect(parser.extract_data_sets(test_row)).to eq([["from", "AUD"], ["to", "CAD"], ["conversion", "1.0079"]])
+  end
+
+  it 'returns formatted data sets' do
+    parser = XmlParser.new
+    expect(parser.parse_xml('spec/rates.xml')).to eq(
+      [
+        {'from' => 'AUD', 'to' => 'CAD', 'conversion' => '1.0079'},
+        {'from' => 'AUD', 'to' => 'EUR', 'conversion' => '0.7439'},
+        {'from' => 'CAD', 'to' => 'AUD', 'conversion' => '0.9921'},
+        {'from' => 'CAD', 'to' => 'USD', 'conversion' => '1.0090'},
+        {'from' => 'EUR', 'to' => 'AUD', 'conversion' => '1.3442'},
+        {'from' => 'USD', 'to' => 'CAD', 'conversion' => '0.9911'},
+      ])
+  end
+
+end
