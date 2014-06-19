9ba38d3988ef7b9cb469315eefe62486f53c931e
calvached <calvache.diana@gmail.com>
2014-06-18 23:05:39 -0500

    Changing tests for different data input and adding more expectations for bankers round

--- spec/currency_converter_spec.rb
+++ spec/currency_converter_spec.rb
@@ -1,26 +1,28 @@
 require 'currency_converter'
 
 describe CurrencyConverter do
-  let(:converter) { CurrencyConverter.new('spec/rates.xml') }
+  let(:converter) { CurrencyConverter.new }
 
-  it 'converts currency' do
-    sale = {'store' => 'Nashua', 'sku' => 'DM1182', 'amount' => 60.10, 'origin' => 'CAD'}
-    sale_2 = {'store' => 'Nashua', 'sku' => 'DM1182', 'amount' => 50.00, 'origin' =>'AUD'}
-    expect(converter.convert(sale, 'USD')).to eq({'store' => 'Nashua', 'sku' => 'DM1182', 'amount' => 60.64, 'origin' => 'USD'})
-    expect(converter.convert(sale_2, 'EUR')).to eq({'store' => 'Nashua', 'sku' => 'DM1182', 'amount' => 37.2, 'origin' => 'EUR'})
+  it 'converts currency when direct conversion rate exists' do
+    expect(converter.convert(60.10, 'CADUSD')).to eq(60.64)
+    expect(converter.convert(50.00, 'AUDEUR')).to eq(37.2)
+  end
+
+  it "converts currency to CAD, then to destination country when direct converation rate doesn't exist" do
+    expect(converter.convert(2.30, 'AUDUSD')).to eq(2.34)
   end
 
   it 'returns a converted amount' do
-    rate = {"from"=>"AUD", "to"=>"CAD", "conversion" => 2.00}
-    sale = {"store"=>"Nashua", "sku"=>"DM1182", "amount" => 60.00, 'origin' => 'AUD'}
-
-    expect(converter.get_converted_amount(sale, rate)).to eq(120.00)
+    expect(converter.get_converted_amount(60.00, 2.00)).to eq(120.00)
   end
 
   it 'returns a rounded amount using bankers round' do
     expect(converter.bankers_round(54.1754)).to eq(54.18)
     expect(converter.bankers_round(343.2050)).to eq(343.20)
     expect(converter.bankers_round(106.2038)).to eq(106.20)
+    expect(converter.bankers_round(134.229245622)).to eq(134.22)
+    expect(converter.bankers_round(59.574167038)).to eq(59.57)
+    expect(converter.bankers_round(20.013991248)).to eq(20.01)
   end
 
   it 'returns a standard rounded amount' do
