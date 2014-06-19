2b1757bb178138aff3e218b926fe9659a38350f4
calvached <calvache.diana@gmail.com>
2014-06-18 23:01:22 -0500

    Making changes for different data intake and fixing bug in bankers round

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,37 +1,42 @@
+require_relative 'xml_parser'
+
 class CurrencyConverter
-  def initialize(file)
-    @rates = XmlParser.new.parse_xml(file)
+  def initialize
+    @rates = XmlParser.new.parse_xml('lib/rates.xml')
   end
 
-  def convert(sale, destination_country)
-    @rates.each do |rate|
-      if sale['origin'] == rate['from'] && destination_country == rate['to']
-        converted_amount = get_converted_amount(sale, rate)
-
-        sale['amount'] = bankers_round(converted_amount) 
-        sale['origin'] = destination_country
-      end
+  def convert(amount, conversion)
+    if @rates[conversion]
+      converted_amount = get_converted_amount(amount, @rates[conversion])
+      bankers_round(converted_amount)
+    else
+      converted_cad = get_converted_amount(amount, @rates[conversion[0..-4] + 'CAD'])
+      converted_amount = get_converted_amount(converted_cad, @rates['CADUSD'])
+      bankers_round(converted_amount)
     end
-
-    sale
   end
 
-  def get_converted_amount(sale, rate)
-    rate['conversion'] * sale['amount']
+  def get_converted_amount(amount, rate)
+    amount * rate
   end
 
   def bankers_round(amount)
-    rounded_amount = standard_round(amount)
+    three_decimal_amount = round_to_three_decimals(amount)
+    rounded_amount = standard_round(three_decimal_amount)
 
-    if last_two_digits(rounded_amount).even?
+    if last_digits(rounded_amount).even? || last_digits(three_decimal_amount).to_s[-1,1].to_i < 5
       rounded_amount
     else
       rounded_amount - 0.01
     end
   end
 
-  def last_two_digits(amount)
+  def last_digits(amount)
     amount.to_s.split('.')[1].to_i
+  end
+
+  def round_to_three_decimals(amount)
+    (amount * 1000).round / 1000.0
   end
 
   def standard_round(amount)
