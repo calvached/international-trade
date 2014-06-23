75bf0cb8f2c7b9faa64bc36dcc9e14c87f9a33b0
calvached <calvache.diana@gmail.com>
2014-06-23 14:10:01 -0500

    Refactoring specs

--- spec/sales_spec.rb
+++ spec/sales_spec.rb
@@ -4,12 +4,10 @@ describe Sales do
   let(:sales) { Sales.new }
 
   it 'adds all the sales from a given sku' do
-    rate = {'USDUSD' => 2}
-    converter = instance_double("CurrencyConverter", :rates => rate)
-    sales.converter = converter
+    sales.converter = instance_double("CurrencyConverter", :rates => {'USDUSD' => 2})
     sales.sales = [{"store"=>"Yonkers", "sku"=>"DM1182", "amount"=>70.0, "origin"=>"USD"}]
-    allow(converter).to receive(:convert).with(70.0, 'USDUSD').and_return(160.0)
+    allow(sales.converter).to receive(:convert).with(70.0, 'USDUSD').and_return(140.0)
 
-    expect(sales.total('DM1182')).to eq(160.0)
+    expect(sales.total('DM1182')).to eq(140.0)
   end
 end
